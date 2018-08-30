a5f3acb5fe44ffc1195f147ca683277f94ac584d
isandison <42772662+isandison@users.noreply.github.com>
2018-08-30 14:49:53 +0100

    Completed the module

---
+++ GradeBook/StandardGradeBook.cs
@@ -0,0 +1,13 @@
+﻿using GradeBook.Enums;
+using GradeBook.GradeBooks;
+
+namespace GradeBook
+{
+    public class StandardGradeBook : BaseGradeBook
+    {
+        public StandardGradeBook(string name) : base(name)
+        {
+            Type = GradeBookType.Standard;
+        }
+    }
+}
