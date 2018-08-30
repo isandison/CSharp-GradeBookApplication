3f2954f12ba0d35014800ebc041179b950531597
isandison <42772662+isandison@users.noreply.github.com>
2018-08-30 15:52:43 +0100

    Completed the module

--- GradeBook/RankedGradeBook.cs
+++ GradeBook/RankedGradeBook.cs
@@ -1,4 +1,5 @@
-﻿using GradeBook.Enums;
+﻿using System;
+using GradeBook.Enums;
 using GradeBook.GradeBooks;
 
 namespace GradeBook
@@ -8,6 +9,25 @@ namespace GradeBook
         public RankedGradeBook(string name) : base(name)
         {
             Type = GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException(message: "not enough students");
+            }
+            if (averageGrade >= 80)
+                return 'A';
+            else if (averageGrade < 80 && averageGrade >= 60)
+                return 'B';
+            else if (averageGrade < 60 && averageGrade >= 40)
+                return 'C';
+            else if (averageGrade < 40 && averageGrade >= 20)
+                return 'D';
+
+            return 'F';
+
         }
     }
 }
